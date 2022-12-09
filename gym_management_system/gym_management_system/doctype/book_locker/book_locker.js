907514f4c93abbc7d25c44eeb4ae1473e2226588
Shwetabolaj <shweta.chougule@erpdata.in>
2022-12-09 08:43:54 +0100

    commit

--- gym_management_system/gym_management_system/doctype/book_locker/book_locker.js
+++ gym_management_system/gym_management_system/doctype/book_locker/book_locker.js
@@ -2,7 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Book Locker', {
-	// refresh: function(frm) {
-
-	// }
+	 refresh: function(frm) {
+		frm.fields_dict['available_lockers'].get_query = function(doc) {
+			return {
+				filters: {
+					"is_available": '1'
+				}
+			}
+		}
+	}
 });
--- gym_management_system/gym_management_system/doctype/gym_member/gym_member.py
+++ gym_management_system/gym_management_system/doctype/gym_member/gym_member.py
@@ -7,6 +7,7 @@ from frappe.model.document import Document
 class GymMember(Document):
 	def on_update(self):
 		self.create_user()
+		self.create_user_permission()
 
 	def create_user(self):
 		frappe.get_doc({
@@ -23,5 +24,14 @@ class GymMember(Document):
 		
 	def before_save(self):
 		self.full_name = f'{self.first_name} {self.last_name}'
+
+	def create_user_permission(self):
+		frappe.get_doc({
+			"doctype":"User Permission",
+			"user":self.email,
+			"allow":"Gym Member",
+			"for_value":self.first_name +" "+self.last_name+"-"+self.phone,
+			"apply_to_all_doctypes":1,
+		}).insert(ignore_permissions=True)
 
 	
